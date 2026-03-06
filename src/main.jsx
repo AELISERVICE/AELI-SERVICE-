e531cb38983ea5b47975abea5500509e7b1063f3
otbludo <otabelaludovic6@gmail.com>
2026-03-06 07:25:22 +0100

    Ajout HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
+import { HashRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes' // <--- Import
 import { GlobalLoadingProvider } from './context/GlobalLoadingContext'
@@ -11,12 +12,12 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="light"> {}
+    <ThemeProvider attribute="class" defaultTheme="light"> { }
       <QueryClientProvider client={queryClient}>
         <GlobalLoadingProvider>
-          <BrowserRouter>
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
         </GlobalLoadingProvider>
       </QueryClientProvider>
     </ThemeProvider>
