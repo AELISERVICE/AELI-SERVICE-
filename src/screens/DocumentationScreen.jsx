3f34ff2ce9cbcacd58daa7c4a4dfab25d41b8812
otbludo <otabelaludovic6@gmail.com>
2026-04-01 20:36:31 +0100

    mise a jour

--- src/screens/DocumentationScreen.jsx
+++ src/screens/DocumentationScreen.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-
-
 export function DocumentationScreen() {
     return (
-        <div className="h-[75vh] md:h-[80vh]">
-            <iframe src="./aeli-service-guide-utilisation.pdf" style={{ width: "100%", height: "100%", backgroundColor: "#ffffff" }} frameborder="0" />
+        <div className="relative z-10 h-[75vh] md:h-[80vh]">
+            <iframe
+                src="./aeli-service-guide-utilisation.pdf"
+                style={{ width: "100%", height: "100%", backgroundColor: "#ffffff" }}
+                frameBorder="0"
+            />
         </div>
     )
-} 
+}
