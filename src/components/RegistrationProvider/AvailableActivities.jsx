99cfc617e4bfdfde6ca509fd629331fd121834c6
otbludo <otabelaludovic6@gmail.com>
2026-04-26 13:16:22 +0100

    mise a jour liste des activites

--- src/components/RegistrationProvider/AvailableActivities.jsx
+++ src/components/RegistrationProvider/AvailableActivities.jsx
@@ -1,58 +1,53 @@
 export const availableActivities = [
     // --- AGRICULTURE, AGRO-ALIMENTAIRE & ÉLEVAGE ---
-    "Agriculture (Maraîchage / Vivriers)", "Élevage (Aviculture, Porciculture, Pisciculture)",
-    "Transformation agro-alimentaire ",
+    "AGRO-ALIMENTAIRE ", "AGro-pastoral", "Agriculture (Maraîchage / Vivriers)",
+    "Élevage (Aviculture, Porciculture, Pisciculture)", "Transformation agro-alimentaire ",
     "Production de jus et boissons locales", "Apiculture", "Vente d'intrants agricoles",
-    "Pépinière et horticulture", "Transformation d'huile de palme", "AGRO-ALIMENTAIRE ",
-    "AGro-pastoral",
+    "Pépinière et horticulture", "Transformation d'huile de palme",
 
     // --- CONSTRUCTION, BTP & IMMOBILIER ---
-    "Maçonnerie", "Plomberie", "Électricité bâtiment", "Menuiserie (Bois/Alu/Métal)",
-    "Peinture et décoration intérieure", "Architecture", "Géomètre-expert",
-    "Vente de matériaux de construction", "Fabrication de parpaings", "Serrurerie",
+    "construction et immobilier", "Maçonnerie", "Plomberie", "Électricité bâtiment",
+    "Menuiserie (Bois/Alu/Métal)", "Peinture et décoration intérieure", "Architecture",
+    "Vente de matériaux de construction", "Fabrication de parpaings", "Serrurerie", "Géomètre-expert",
     "Carrelage", "Étanchéité", "Promotion immobilière", "Gestion locative",
     "Location d'engins de chantier", "Rénovation et réhabilitation", "Construction métallique",
     "Isolation thermique", "Aménagement paysager", "Construction écologique", "Démolition et désamiantage",
-    "Topographie et levés géodésiques", "Construction modulaire", "construction et immobilier",
-
+    "Topographie et levés géodésiques", "Construction modulaire",
 
     // --- TECHNOLOGIE, DIGITAL & SERVICES B2B ---
-    "Développement Web et Mobile", "Graphisme et Design", "Community Management",
+    "Informatique et services numériques", "Développement Web et Mobile", "Graphisme et Design",
     "Rédaction Web et Copywriting", "Référencement (SEO/SEA)", "Cybersécurité",
     "Data Analysis", "Installation réseau et fibre", "Réparation informatique",
     "Vente de matériel informatique", "Marketing digital", "Conseil en stratégie",
     "Comptabilité et Expertise comptable", "Secrétariat externalisé", "RH et Recrutement",
     "Assistance juridique", "Traduction et Interprétariat", "Nettoyage industriel",
-    "Sécurité et Gardiennage", "Impression et reprographie", "Informatique et services numériques",
+    "Sécurité et Gardiennage", "Impression et reprographie", "Community Management",
     "Services aux entreprises", "Consulting et formation professionnelle",
 
     // --- COMMERCE & DISTRIBUTION ---
-    "Commerce de détail", "Supermarché", "Friperie", "Vente de vêtements neufs",
-    "Vente de pièces détachées", "Vente d'électroménager",
-    "Vente de cosmétiques et produits de beauté", "Vente de produits pharmaceutiques",
+    "Commerce & Distribution", "Commerce de détail", "Supermarché", "Friperie", "Vente de vêtements",
+    "Vente d'électroménager", "Vente de cosmétiques et produits de beauté", "Vente de produits pharmaceutiques",
     "Quincaillerie", "Vente de meubles", "Vente de produits électroniques/téléphonie",
-    "Agence de voyage", "Commerce & Distribution",
 
     // --- SANTÉ, BIEN-ÊTRE & FORMATION ---
-    "Coiffure", "Esthétique et Onglerie", "Massage et Spa", "Coaching Sportif",
-    "Nutrition et Diététique", "Psychologie", "Clinique médicale/dentaire",
+    "Santé", "Bien-être", "Coiffure", "Esthétique et Onglerie", "Massage et Spa",
+    "Coaching Sportif", "Nutrition et Diététique", "Psychologie", "Clinique médicale/dentaire",
     "Laboratoire d'analyses médicales", "Cours d'appui / Soutien scolaire",
     "Établissement d'enseignement privé", "Centre de formation professionnelle",
-    "Formation en langues", "Santé", "Bien-être",
+    "Formation en langues", "Agence de voyage",
 
     // --- TRANSPORT, LOGISTIQUE & ÉNERGIE ---
-    "Transport (Taxi / Moto-taxi)", "Location de véhicules", "Transport de marchandises",
-    "Logistique et Transit", "Livraison à domicile (Coursier)", "Auto-école",
+    "Transport", "Logistique", "Energie", "Transport (Taxi / Moto-taxi)", "Location de véhicules",
+    "Logistique et Transit", "Livraison à domicile (Coursier)", "Auto-école", "Transport de marchandises",
     "Lavage auto et entretien", "Mécanique auto/moto", "Installation solaire",
     "Vente de gaz domestique", "import-export", "Dépannage et remorquage",
-    "TRANSPORT", "LOGISTIQUE", "ÉNERGIE",
 
     // --- ÉVÉNEMENTIEL, CRÉATION & TOURISME ---
-    "Photographie et Vidéographie", "Animation DJ", "Traiteur (Catering)",
+    "Evénementiels", "Création", "Tourisme", "Photographie et Vidéographie",
     "Organisation de mariages/événements", "Décoration événementielle",
     "Fleuriste", "Hôtellerie", "Restauration (Restaurant/Fast-food)", "Bar/Lounge",
     "Agence de tourisme", "Arts plastiques et artisanat local", "Couture et Stylisme",
-    "Evénementiels", "Création", "Tourisme",
+    "Animation DJ", "Traiteur (Catering)",
 
     // --- AUTRES ---
     "Autres"
